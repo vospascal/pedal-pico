e89066b1420eb5bd5ebed50bf034128c753d6e5b
OrhanYigitDurmaz <orhanyigitv2durmaz@gmail.com>
2023-05-27 21:14:46 +0300

    Update SoftwareReset.h

--- PedalBox/src/SoftwareReset/SoftwareReset.h
+++ PedalBox/src/SoftwareReset/SoftwareReset.h
@@ -1,26 +1,25 @@
+//https://github.com/OrhanYigitDurmaz/rp2040-libraries
 #include "pico/stdlib.h"
-#include "hardware/watchdog.h"
 
 namespace softwareReset
 {
-    inline void simple()
-    {
-        asm volatile(" jmp 0");
-    }
+  inline void simple()
+  {
+    reset_usb_boot(0, 0);
+    reset_cpu();
+  }
 
-    inline void standard()
+  inline void standard()
+  {
+    while (true)
     {
-        do
-        {
-            watchdog_enable(15 * 1000, true); // Enable the watchdog with a timeout of 15 milliseconds
-            sleep_ms(UINT32_MAX);       // Enter an infinite sleep loop
-
-        } while (0);
+      // Perform any cleanup tasks if needed
+      
+      // Reset the RP2040 by calling reset_usb_boot()
+      reset_usb_boot(0, 0);
     }
+  }
 
-    void disable()
-    {
-        watchdog_reboot(0, 0, 0); // Reset the watchdog timer
-        watchdog_enable(0, false); // Disable the watchdog
-    }
+  void disable() { }
 }
+
